2c411e5959f5a822e90c86d166570a4b9a46f882
Dorsey B <dorseybec@gmail.com>
2023-04-16 16:10:16 -0700

    Compelte

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        // debugger; // commands: cont(Continue)
+    }
+
+    return gifts;
+}
+
+function writeCards(arr,eventName) {
+    let returnArr = [];
+    for (let index = 0; index < arr.length; index++) {
+        returnArr[index] = `Thank you, ${arr[index]}, for the wonderful ${eventName} gift!`;
+        //Alternatively could use push or unshift
+    }
+    return returnArr
+}
+
+function countDown(value) {
+    let tmp = value;
+    while (tmp > -1 ) {
+        console.log(tmp--);
+    }
+    
+}
+
